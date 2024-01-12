a95b10f9960e2c029565fbea4fb203e41d28c4f3
Anirban Kapil <anirbankapil@gmail.com>
2024-01-12 12:41:10 +0530

    Frist Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const parent = React.createElement(
+    "div",{id : "parent" },
+    React.createElement("div",{id : "child 1"},
+    [React.createElement("h1",{},"I am an H1 tag"),
+    React.createElement("h2",{},"I am H2 tag")],
+    React.createElement("div",{id : "child 2"},
+    [React.createElement("h1",{},"I am an H1 tag"),
+    React.createElement("h2",{},"I am H2 tag")])
+        
+    
+    )
+    );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
